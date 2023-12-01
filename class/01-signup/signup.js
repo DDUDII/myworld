d3f0dbab6480910283153b7cd424294954d9f003
DDUDII <qortn121@naver.com>
2023-12-01 20:25:55 +0900

    feat: 이메일 유효성 검사 추가
    feat: 이메일 유효성 검사 추가

--- class/01-signup/signup.js
+++ class/01-signup/signup.js
@@ -2,7 +2,30 @@ const emailInput = document.getElementById("emailInput");
 const nameInput = document.getElementById("nameInput");
 const passwordInput = document.getElementById("passwordInput");
 const passwordConfirmInput = document.getElementById("passwordConfirmInput");
-const gender = document.getElementById("gender");
+const female = document.getElementById("female");
+const male = document.getElementById("male");
+const other = document.getElementById("other");
 const agreedCheck = document.getElementById("agreedCheck");
 const signupBtn = document.getElementById("signupBtn");
-const returnBtn = document.getElementById("returnBtn");
+const emailWarning = document.getElementById("emailWarning");
+const nameWarning = document.getElementById("nameWarning");
+const passwordWarning = document.getElementById("passwordWarning");
+const passwordConfirmWarning = document.getElementById(
+  "passwordConfirmWarning"
+);
+const genderWarning = document.getElementById("genderWarning");
+const agreedCheckWarning = document.getElementById("agreedCheckWarning");
+
+const isEmailValid = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+signupBtn.addEventListener("click", () => {
+  const emailValue = emailInput.value;
+
+  if (!isEmailValid(emailValue)) {
+    alert("올바른 이메일 형식이 아닙니다.");
+    return;
+  }
+});
